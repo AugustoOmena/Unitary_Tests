5b3cfded58e8b03f0071c345ad181c380a69a338
Augusto Omena <augusto.omena.n@gmail.com>
2024-02-14 22:25:38 -0300

    Add more functions.

--- my-code.js
+++ my-code.js
@@ -3,4 +3,16 @@ const sum = (a, b) => {
     return a + b
 }
 
-module.exports = {sum}
+const sub = (a, b) => {
+    return a - b
+}
+
+const mult = (a, b) => {
+    return a * b
+}
+
+const div = (a, b) => {
+    return a / b
+}
+
+module.exports = {sum, sub, mult, div}
--- my-code.test.js
+++ my-code.test.js
@@ -1,5 +1,22 @@
 const { sum } = require('./my-code')
+const { sub } = require('./my-code')
+const { mult } = require('./my-code')
+const { div } = require('./my-code')
 
-it('sums 2 numbers', () => {
-    expect(sum(1,2)).toBe(3)
+describe('time functions', () => {
+    it('sums 2 numbers', () => {
+        expect(sum(1,2)).toBe(3)
+    })
+
+    it('subs 2 numbers', () => {
+        expect(sub(2,1)).toBe(1)
+    })
+
+    it('mult 2 numbers', () => {
+        expect(mult(2,2)).toBe(4)
+    })
+
+    it('div 2 numbers', () => {
+        expect(div(2,2)).toBe(1)
+    })
 })
